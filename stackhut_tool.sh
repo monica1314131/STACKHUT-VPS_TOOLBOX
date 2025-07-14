99aad56497605896e1a342c86d59d89f592c71a2
monica1314131 <monica1314131@gmail.com>
2025-07-14 07:50:52 -0700

    🔄 更新脚本文件

--- stackhut_tool.sh
+++ stackhut_tool.sh
@@ -657,17 +657,17 @@ nodes_menu() {
   while true; do
     clear
     echo -e "${YELLOW}▶ 节点搭建${RESET}"
-    echo -e "${GREEN}------------sing-box------------${RESET}"
+    echo -e "${GREEN}------------sing-box------------"
     echo " 1. 233boy.sing-box一键脚本"
     echo " 2. YGKKK-Sing-box四合一"
     
-    echo -e "${GREEN}------------XRAY面板------------${RESET}"
+    echo -e "------------XRAY面板------------"
     echo " 3. X-UI面板(原版)"
     echo " 4. YGKKK-X-UI面板"
     echo " 5. 3X-UI面板(优化版)"
     echo " 6. 3X-UI面板(alpine系统专用)"
     
-    echo -e "${GREEN}-----------一键添加IPV4出口-------------${RESET}"
+    echo -e "-----------一键添加IPV4出口-------------"
     echo " 7. Alice免费机一键添加IPV4出口"
     
     echo "------------------------"
